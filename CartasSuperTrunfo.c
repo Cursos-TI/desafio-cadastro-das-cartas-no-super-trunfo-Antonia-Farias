915785fe308b0bfdc05cf235a91d89ab211581dc
Antonia-Farias <antonia.fariass@gmail.com>
2025-05-14 00:20:24 +0000

    Estou colocando as variaveis

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,5 +3,18 @@
 int main(){
     printf("Desafio Super Trunfo\n");
     printf("Novo commit\n");
+
+    char codigo da cidade;
+    char[20] nome;
+    int populacao;
+    float area em km;
+    float PIB;
+    int quantidade ponto turistico;
+
+    printf("Digite o codigo da cidade: \n");
+    scanf("%d", &codigo da cidade);
+
+    printf("Digite o nome: \n");
+    scanf("%s", &nome);
    
 }
