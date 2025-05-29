6ac5dee24fda4796d46c678fe62827a8a7f9fefa
Antonia-Farias <antonia.fariass@gmail.com>
2025-05-29 00:12:49 +0000

    melhorando meu codigo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -14,7 +14,7 @@ int PontosTuristicos;
 } Carta;
 
 float calcularDensidadePopulacional (Carta carta){
-    return carta.pib / carta.area;
+    return carta.populacao / carta.area;
 }
 
 float calcularPibPerCapita (Carta carta) {
@@ -22,10 +22,19 @@ float calcularPibPerCapita (Carta carta) {
 }
 
 void compararCartas (Carta carta1 , Carta carta2) {
-     float densidade1 = calcularDensidadePopulacional1(carta1);
-     float densidade2 = calcularDensidadePopulacional2(carta2);
+     float densidade1 = calcularDensidadePopulacional(carta1);
+     float densidade2 = calcularDensidadePopulacional(carta2);
 
      printf("Comparando a densidade populacional: \n");
-     printf("s (%s): Densidade = %.2f\n" , carta1.NomeCidade, carta1.estado, densidade1);
-     printf("s (%s): Densidade = %.2f\n" , carta2.NomeCidade, carta2.estado, densidade2);
+     printf("%s (%s): Densidade = %.2f\n" , carta1.NomeCidade, carta1.estado, densidade1);
+     printf("%s (%s): Densidade = %.2f\n" , carta2.NomeCidade, carta2.estado, densidade2);
+}
+
+int main () {
+    Carta carta1 = {"São Paulo", "SP01" , "São Paulo" , 12300000, 1521.11, 2300000000};
+    Carta carta2 = {"Rio de Janeiro", "RJ01", "Rio de Janeiro" , 6748000, 1182.30, 1500000000.0, 45};
+
+    compararCartas(carta1 , carta2);
+
+    return 0;
 }
